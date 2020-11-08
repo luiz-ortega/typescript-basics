6978da84b0ef606854cdc5f95d9c33b3bcd2ffd4
luiz-ortega <luizngust@hotmail.com>
2020-11-08 13:44:49 -0300

    feat: basic typing.

---
+++ index.js
@@ -0,0 +1,9 @@
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+var button = document.getElementById("button");
+function sum(a, b) {
+    return Number(a) + Number(b);
+}
+button.addEventListener("click", function () {
+    console.log(sum(input1.value, input2.value));
+});
---
+++ index.ts
@@ -0,0 +1,11 @@
+const input1 = document.getElementById("num1") as HTMLInputElement;
+const input2 = document.getElementById("num2") as HTMLInputElement;
+const button = document.getElementById("button");
+
+function sum(a: string, b: string) {
+  return Number(a) + Number(b);
+}
+
+button.addEventListener("click", function () {
+  console.log(sum(input1.value, input2.value));
+});
